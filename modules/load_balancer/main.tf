b5deed2ba759c6b427d059317ce1be15fcf1cbda
khond <44898876+khond@users.noreply.github.com>
2024-08-19 18:24:35 +0400

    Create main.tf

---
+++ modules/load_balancer/main.tf
@@ -0,0 +1,41 @@
+resource "aws_lb" "web_alb" {
+  name               = "web-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.security_group_id]
+  subnets            = var.subnet_ids
+
+  enable_deletion_protection = false
+  idle_timeout               = 400
+
+  tags = {
+    Name = "web-alb"
+  }
+}
+
+resource "aws_lb_target_group" "web_tg" {
+  name        = "web-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "instance"
+}
+
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.web_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
+
+output "alb_arn" {
+  value = aws_lb.web_alb.arn
+}
+
+output "target_group_arn" {
+  value = aws_lb_target_group.web_tg.arn
+}
