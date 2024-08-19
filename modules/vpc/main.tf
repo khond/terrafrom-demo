269ffdb50f8351b2f74b164c408bfec3b4fd27c2
khond <44898876+khond@users.noreply.github.com>
2024-08-19 18:18:47 +0400

    Create main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,55 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.main_vpc.id
+}
+
+output "public_route_table_id" {
+  value = aws_route_table.public_rt.id
+}
+
+output "private_route_table_id" {
+  value = aws_route_table.private_rt.id
+}
